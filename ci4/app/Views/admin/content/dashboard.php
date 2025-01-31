98c1a716243882782bd35f90dabc3bbd5c7658cc
Suluh Arwani <suluharwani007@gmail.com>
2025-01-31 16:27:44 +0700

    update

--- ci4/app/Views/admin/content/dashboard.php
+++ ci4/app/Views/admin/content/dashboard.php
@@ -1,3 +1,8 @@
+<link rel="stylesheet" type="text/css" href="<?=base_url('assets')?>/datatables/datatables.min.css"/>
+<link rel="stylesheet" type="text/css" href="<?=base_url('assets')?>/summernote/summernote-image-list.min.css"/>
+<link rel="stylesheet" type="text/css" href="<?=base_url('assets')?>/summernote/summernote-lite.min.css"/>
+
+
 <div class="page-content">
     <section class="row">
         <div class="col-12 col-lg-9">
@@ -84,114 +89,42 @@
                 </div>
             </div>
             <div class="row">
-                <div class="col-12 col-xl-4">
-                    <div class="card">
-                        <div class="card-header">
-                            <h4>Profile Visit</h4>
-                        </div>
-                        <div class="card-body">
-                            <div class="row">
-                                <div class="col-6">
-                                    <div class="d-flex align-items-center">
-                                        <svg class="bi text-primary" width="32" height="32" fill="blue"
-                                            style="width:10px">
-                                            <use
-                                                xlink:href="<?=base_url('assets/template/dist')?>/assets/images/bootstrap-icons.svg#circle-fill" />
-                                        </svg>
-                                        <h5 class="mb-0 ms-3">Europe</h5>
-                                    </div>
-                                </div>
-                                <div class="col-6">
-                                    <h5 class="mb-0">862</h5>
-                                </div>
-                                <div class="col-12">
-                                    <div id="chart-europe"></div>
-                                </div>
-                            </div>
-                            <div class="row">
-                                <div class="col-6">
-                                    <div class="d-flex align-items-center">
-                                        <svg class="bi text-success" width="32" height="32" fill="blue"
-                                            style="width:10px">
-                                            <use
-                                                xlink:href="<?=base_url('assets/template/dist')?>/assets/images/bootstrap-icons.svg#circle-fill" />
-                                        </svg>
-                                        <h5 class="mb-0 ms-3">America</h5>
-                                    </div>
-                                </div>
-                                <div class="col-6">
-                                    <h5 class="mb-0">375</h5>
-                                </div>
-                                <div class="col-12">
-                                    <div id="chart-america"></div>
-                                </div>
-                            </div>
-                            <div class="row">
-                                <div class="col-6">
-                                    <div class="d-flex align-items-center">
-                                        <svg class="bi text-danger" width="32" height="32" fill="blue"
-                                            style="width:10px">
-                                            <use
-                                                xlink:href="<?=base_url('assets/template/dist')?>/assets/images/bootstrap-icons.svg#circle-fill" />
-                                        </svg>
-                                        <h5 class="mb-0 ms-3">Indonesia</h5>
-                                    </div>
-                                </div>
-                                <div class="col-6">
-                                    <h5 class="mb-0">1025</h5>
-                                </div>
-                                <div class="col-12">
-                                    <div id="chart-indonesia"></div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+               
                 <div class="col-12 col-xl-8">
                     <div class="card">
                         <div class="card-header">
                             <h4>Latest Comments</h4>
                         </div>
                         <div class="card-body">
-                            <div class="table-responsive">
-                                <table class="table table-hover table-lg">
-                                    <thead>
-                                        <tr>
-                                            <th>Name</th>
-                                            <th>Comment</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        <tr>
-                                            <td class="col-3">
-                                                <div class="d-flex align-items-center">
-                                                    <div class="avatar avatar-md">
-                                                        <img src="<?=base_url('assets/template/dist')?>/assets/images/faces/5.jpg">
-                                                    </div>
-                                                    <p class="font-bold ms-3 mb-0">Cantik</p>
-                                                </div>
-                                            </td>
-                                            <td class="col-auto">
-                                                <p class=" mb-0">Congratulations on your graduation!</p>
-                                            </td>
-                                        </tr>
-                                        <tr>
-                                            <td class="col-3">
-                                                <div class="d-flex align-items-center">
-                                                    <div class="avatar avatar-md">
-                                                        <img src="<?=base_url('assets/template/dist')?>/assets/images/faces/2.jpg">
-                                                    </div>
-                                                    <p class="font-bold ms-3 mb-0">Ganteng</p>
-                                                </div>
-                                            </td>
-                                            <td class="col-auto">
-                                                <p class=" mb-0">Wow amazing design! Can you make another tutorial for
-                                                    this design?</p>
-                                            </td>
-                                        </tr>
-                                    </tbody>
-                                </table>
-                            </div>
+                        <div class="table-responsive">
+            <table id="tabel_serverside" class="table table-bordered display text-left" cellspacing="0" width="100%">
+              <thead>
+                <tr  class="text-center">
+                  <th style="width: 5%; text-align: center;">NO</th>
+                  <th style="width: 5%; text-align: center;">TEAM</th>
+                  <th style="width: 5%; text-align: center;">KODE</th>
+                  <th style="width: 5%; text-align: center;">CUSTOMER</th>
+                  <th style="width: 5%; text-align: center;">DEADLINE</th>
+                  <th style="width: 5%; text-align: center;">QTY</th>
+                  <th style="width: 5%; text-align: center;">STATUS</th>
+                  <th style="width: 20%; text-align: center;">ACTION</th>
+                </tr>
+              </thead>
+              <tfoot>
+                <tr class="text-center">
+                  <th style="width: 5%; text-align: center;">NO</th>
+                  <th style="width: 5%; text-align: center;">TEAM</th>
+                  <th style="width: 5%; text-align: center;">KODE</th>
+                  <th style="width: 5%; text-align: center;">CUSTOMER</th>
+                  <th style="width: 5%; text-align: center;">DEADLINE</th>
+                  <th style="width: 5%; text-align: center;">QTY</th>
+                  <th style="width: 5%; text-align: center;">STATUS</th>
+                  <th style="width: 20%; text-align: center;">ACTION</th>
+                </tr>
+              </tr>
+            </tfoot>
+          </table>
+        </div>
                         </div>
                     </div>
                 </div>
@@ -261,5 +194,9 @@
         </div>
     </section>
 </div>
+<script type="text/javascript" src="<?=base_url('assets')?>/datatables/datatables.min.js"></script>
+<script type="text/javascript" src="<?=base_url()?>/assets/summernote/summernote-lite.min.js"></script>
+<script type="text/javascript" src="<?=base_url()?>/assets/summernote/summernote-image-list.min.js"></script>
 <script src="<?=base_url('assets/template/dist')?>/assets/extensions/apexcharts/apexcharts.min.js"></script>
 <script src="<?=base_url('assets/template/dist')?>/assets/js/pages/dashboard.js"></script>
+<script type="text/javascript" src="<?=base_url()?>/assets/js/dashboardData.js"></script>
